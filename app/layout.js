df7744f48d1abc21d84f53796ca117f1cdab6614
neitantoscano <toscanoneitan@gmail.com>
2026-06-06 19:49:34 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,23 +4,22 @@ export const metadata = {
   title: 'Munchy',
   description: 'Antojos saludables para Gen Z',
   manifest: '/manifest.json',
+}
+
+export const viewport = {
   themeColor: '#2e3a23',
-  viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-        />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Libre+Caslon+Text:wght@400;700&family=DM+Sans:wght@400;500;600;700&display=swap"
           rel="stylesheet"
