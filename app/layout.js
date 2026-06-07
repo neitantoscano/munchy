8f8dffae0dee0a0ddf509db71231cfe2e9a7a8fe
neitantoscano <toscanoneitan@gmail.com>
2026-06-07 14:40:52 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import './globals.css'
+import './globals.css';
 
 export const metadata = {
   title: 'Munchy',
@@ -11,25 +11,13 @@ export const viewport = {
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
-  userScalable: false,
+  userScalable: 'no',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Libre+Caslon+Text:wght@400;700&family=DM+Sans:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className="bg-crema text-olivoOscuro font-sans antialiased">
-        <div className="max-w-md mx-auto min-h-screen relative">
-          {children}
-        </div>
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
